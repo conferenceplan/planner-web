45a56987d1dc0967d790f8b5c8a8029e6ff20444
Henry Balen <henry@grenadinetech.com>
2014-04-21 14:09:31 -0400

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PlannerWeb::Application.routes.draw do
 
-  # filter :locale
+  filter :locale
   # mount Interpret::Engine => "/interpret" # cause the interpret gem has the old way of defining the engine mount
 
   mount PlannerCore::Engine => '/' #, as: 'planner_core'
