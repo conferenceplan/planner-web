4d005b2507c11e4ea3bdc4bd99d3ea247f7cc623
Henry Balen <henry@grenadinetech.com>
2014-04-25 16:33:52 -0400

    fixes

--- config/initializers/secret_init.rb
+++ config/initializers/secret_init.rb
@@ -1,8 +1,8 @@
 #
 require 'securerandom'
 
-def secure_init
-  token_file = Rails.root.join('.secret')
+def secure_token
+  token_file = Rails.root.join('tmp/.secret')
   if File.exist?(token_file)
     # Use the existing token.
     File.read(token_file).chomp
