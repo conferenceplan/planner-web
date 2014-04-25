ffaca8019b4d87d88a32855195a77a1550402aaa
Henry Balen <henry@grenadinetech.com>
2014-04-25 16:31:05 -0400

    fix file name

---
+++ config/initializers/secret_init.rb
@@ -0,0 +1,17 @@
+#
+require 'securerandom'
+
+def secure_init
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+PlannerWeb::Application.config.secret_token = secure_token
