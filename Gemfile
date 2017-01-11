d3c9915ffcbd8d6f8212926cc1ff1b79ffb67c51
balen <henry@grenadinetech.com>
2017-01-11 11:08:03 -0500

    incorporate gem fixes for R4.2

--- Gemfile
+++ Gemfile
@@ -1,60 +1,39 @@
 source "https://rubygems.org"
 #
-# Needed for installs
 #
-gem "rack", "1.4.5"
+gem "rack"
 gem 'rake'
-gem 'therubyracer',           :platforms => :ruby
-gem 'libv8', '3.16.14.7', :platforms => :ruby
-gem 'puma',                   :platforms => :ruby
-gem 'turbo-sprockets-rails3'
+gem 'therubyracer',                   :platforms => :ruby
+gem 'libv8',            '3.16.14.15', :platforms => :ruby
+gem 'puma',                           :platforms => :ruby
 
-gem 'nokogiri', '1.6.2.1'
-gem "mysql2", "0.3.20"
+gem 'sass-rails',       '~> 5.0.0'
+gem 'uglifier',         '~> 2.7.2'
+gem 'coffee-rails',     '~> 4.1.0'
 
-gem 'bootstrap-sass', '3.1.1.0' #'~> 3.1'
+gem 'nokogiri',         '1.6.2.1'
+gem "mysql2",           "0.3.20"
 
-gem 'enumerations_mixin', :git => 'git://github.com/balen/enumerations_mixin.git'
-
-gem 'acts-as-taggable-on', "3.1.1"
-
-gem 'i18n-active_record',
-      :git => 'git://github.com/svenfuchs/i18n-active_record.git',
-      :tag => 'rails-3.2',
-      :require => 'i18n/active_record'
-
-group :staging, :development do
-  gem 'interpret', :git => 'git://github.com/balen/interpret.git'
-end
-
-# For locale in the route
+gem 'i18n-active_record', :require => 'i18n/active_record'
 
 gem "daemons"
 gem 'delayed_job' 
 gem 'delayed_job_active_record'
-
-gem "figaro"
-
-gem 'geocoder'
-gem "select2-rails", '3.5.9.3'
+gem "select2-rails",    '3.5.9.3'
 gem 'country_select', github: 'stefanpenner/country_select'
+gem 'jbuilder',         '~> 1.2'
 
+group :development do
+  gem 'rb-readline'
+end
 
 # gem 'planner-core', path: "../planner-core"
 # gem 'planner_konopas', path: "../planner_konopas"
 
-gem 'planner-core',     '>=0.1', :git => 'git@github.com:conferenceplan/planner-core.git'
-gem 'planner_konopas',  '>=0.1', :git => 'git@github.com:conferenceplan/planner_konopas.git'
+gem 'planner-core',     '>=0.1', :git => 'git@github.com:conferenceplan/planner-core.git', :branch => 'upgrade/rails42'
+gem 'planner_konopas',  '>=0.1', :git => 'git@github.com:conferenceplan/planner_konopas.git', :branch => 'upgrade/rails42'
 
-# Needed for the new asset pipeline
-group :assets do
-  gem 'sass-rails',   '>= 3.2'
-  gem 'compass-rails'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '= 2.5.1'
-  gem 'yui-compressor'
-end
-
-group :development do
-  gem 'seed_dump', '0.5.3'
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
 end
--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,10 @@ include Log4r
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, Rails.env) # RAILS 4
 end
 
 module PlannerWeb
@@ -24,7 +25,8 @@ module PlannerWeb
     log4r_config= YAML.load_file(File.join(File.dirname(__FILE__),"log4r.yml"))
     YamlConfigurator.decode_yaml( log4r_config['log4r_config'] )
     config.logger = Log4r::Logger[(Rails.env == "development" ? Rails.env : 'planner')]
-
+    
+config.active_record.raise_in_transactional_callbacks = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -71,7 +73,7 @@ module PlannerWeb
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -84,21 +86,22 @@ module PlannerWeb
     #
     # config.middleware.use Rack::JSONP
     
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
+    # TODO - R4.2 check - how to do pre-compile
+    # config.assets.precompile << Proc.new do |path|
+      # if path =~ /\.(css|js)\z/
+        # full_path = Rails.application.assets.resolve(path).to_path
+        # app_assets_path = Rails.root.join('app', 'assets').to_path
+        # if full_path.starts_with? app_assets_path
+          # puts "including asset: " + full_path
+          # true
+        # else
+          # puts "excluding asset: " + full_path
+          # false
+        # end
+      # else
+        # false
+      # end
+    # end
 
     #
     #
